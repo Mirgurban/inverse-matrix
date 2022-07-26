1b3a7b53d272b7b9751f11be53a8e9f098f35390
Mirgurban <64306609+Mirgurban@users.noreply.github.com>
2022-07-26 12:54:18 +0700

    Delete vvod.d

--- MPI_inverse_matrix/vvod.d
+++
@@ -1,4 +0,0 @@
-vvod.o: vvod.cpp /usr/include/x86_64-linux-gnu/mpich/mpi.h \
- /usr/include/x86_64-linux-gnu/mpich/mpio.h \
- /usr/include/x86_64-linux-gnu/mpich/mpi.h \
- /usr/include/x86_64-linux-gnu/mpich/mpicxx.h
