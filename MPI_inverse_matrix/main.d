037089a2989a22d704fd15abf1b6fc489dffe599
Mirgurban <64306609+Mirgurban@users.noreply.github.com>
2022-07-26 12:54:09 +0700

    Delete main.d

--- MPI_inverse_matrix/main.d
+++
@@ -1,4 +0,0 @@
-main.o: main.cpp OBR.h vvod.h /usr/include/x86_64-linux-gnu/mpich/mpi.h \
- /usr/include/x86_64-linux-gnu/mpich/mpio.h \
- /usr/include/x86_64-linux-gnu/mpich/mpi.h \
- /usr/include/x86_64-linux-gnu/mpich/mpicxx.h
